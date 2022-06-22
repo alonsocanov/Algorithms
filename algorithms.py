e0fbd3dc19e6b3354309855d69f512b649b419a8
alonsocanov <alonsocanov@outlook.com>
2022-06-22 10:55:53 -0500

    Contained each type of algorithm in its own filw

--- algorithms.py
+++ algorithms.py
@@ -1,16 +1,3 @@
-from unittest import result
-
-
-def reverse_int(x):
-    '''
-    reverse an integer
-    '''
-    res = 0
-    remain = abs(x)
-    while remain:
-        res = res * 10 + remain % 10
-        remain //= 10
-    return res if x > 0 else -res
 
 
 def pascal_triangle(n):
@@ -22,17 +9,6 @@ def pascal_triangle(n):
         for j in range(1, i):
             result[i][j] = result[i - 1][j - 1] + result[i - 1][j]
     return result
-
-
-def swap_bits(x, i, j):
-    '''
-    check bit parity
-    '''
-    res = x
-    if (x >> i) & 1 != (x >> j) & 1:
-        mask = (1 << i) | (1 << j)
-        res ^= mask
-    return res
 
 
 def sum_exists(array, value):
@@ -57,30 +33,6 @@ def sum_exists(array, value):
         else:
             break
     return False
-
-
-def reverse_word_order(string: str):
-    '''
-    Reverse the order of words in a given sentence
-    time complexity: O(n)
-    memory complexity: O(1)
-    '''
-    idx_end = 0
-    counter = 0
-    lenght = len(string)
-    while counter < lenght:
-        if string[idx_end] == ' ' or idx_end == lenght - 1:
-            word = string[:idx_end]
-            middle_sentence = string[idx_end + 1: lenght - counter + idx_end]
-            end_sentence = string[lenght - counter + idx_end:]
-            string = middle_sentence + ' ' + word + ' ' + end_sentence
-            idx_end = 0
-            counter += 1
-
-        idx_end += 1
-        counter += 1
-
-    return string
 
 
 def mayority_element(array):
@@ -144,38 +96,6 @@ def subarray_max_product(array: list):
     return subarray, product
 
 
-def count_num_bits_to_1(value: int):
-    '''
-     Counting number of bits equal to 1 aims to learn bit shifting algorithms
-    '''
-    count = 0
-    while value:
-        count += value & 1
-        value >>= 1
-    return count
-
-
-def parity_bit_v1(value: int):
-    '''
-    The parity of a word bit is 1 if the number of 1s in a word is odd, o is the number of 1s in a word is even
-    '''
-    result = 0
-    while value:
-        result ^= value & 1
-        value >>= 1
-    return result
-
-
-def parity_bit_v2(value: int):
-    '''
-    The parity of a word bit is 1 if the number of 1s in a word is odd, o is the number of 1s in a word is even
-    '''
-    pow_2 = [32, 16, 8, 4, 2, 1]
-    for i in pow_2:
-        value ^= value >> i
-    return value & 0x1
-
-
 def first_non_repeating(string: str):
     '''
         get unique letters in string
@@ -201,11 +121,4 @@ def longest_sub_string(string: str):
             l += 1
             char_set.add(string[r])
             res = max(res, r - l + 1)
-    return res
-
-
-def single_number(array: list[int]):
-    res = 0
-    for val in array:
-        res ^= val
     return res
