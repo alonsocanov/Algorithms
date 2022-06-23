c82074909bdbc3988e45d0b04d33d3aea712cb72
alonsocanov <alonsocanov@outlook.com>
2022-06-23 09:33:38 -0500

    Fixed max sliding window algorithm

--- sliding_window.py
+++ sliding_window.py
@@ -11,8 +11,19 @@ def lengthOfLongestSubstring(s: str) -> int:
 
 def max_value_in_window(array: list[int], k: int):
     queue = []
-    for val in array:
-        while
+    output = []
+    l, r = 0, 0
+    while r < len(array):
+        while queue and array[queue[-1]] < array[r]:
+            queue.pop()
+        queue.append(r)
+        if l > queue[0]:
+            queue.pop(0)
+        if (r + 1) >= k:
+            output.append(array[queue[0]])
+            l += 1
+        r += 1
+    return output
 
 
 if __name__ == '__main__':
@@ -21,3 +32,11 @@ if __name__ == '__main__':
     print("String:", string)
     length = lengthOfLongestSubstring(string)
     print("Longest substring:", length)
+
+    print("\nSliding window get MAximum of the Window")
+    nums = [1, 2, 3, 5, 6, 1, 3, 6, 4, 7, 4]
+    k = 3
+    print("The array is:", nums)
+    print("The size of the window is:", k)
+    output = max_value_in_window(nums, k)
+    print("The max value of each window is:", output)
